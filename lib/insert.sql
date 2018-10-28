95377c230b17adc8aef657691726a6637062dcb9
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-28 20:30:17 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr. Chocolate",5,"Male","Grey","None",1),
 ("Rowdy",4,"Femal","Grey","Kind",1),
 ("Tabitha",8,"Femal","Grey","Quiet",0),
-("Sergeant Brown",9,"Male","Black","Cool",1),
+("Sergeant Brown",9,"Male","Black","goofy",1),
 ("Melissa",6,"Femal","Brown","",0),
-("Grinch",4,"Femal","Black","Angry",1),
-("Wendy",6,"Male","Brown","Angry",1),
+("Grinch",4,"Femal","Black","goofy",1),
+("Wendy",6,"Male","Brown","Nice",1),
 (NULL,8,"Male","Brown","aggressive",0);
