ec24686bbfb39e5e378a5d7279c7ca880708e2fd
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-28 20:20:35 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr. Chocolate",5,"Male","Grey","",1)
-("Rowdy",4,"Femal","Grey","",1)
-("Tabitha",8,"Femal","Grey","",0)
-("Sergeant Brown",9,"Grey","",1)
-("Melissa",6,"Femal","Brown","",0)
-("Grinch",4,"Femal","Brown","Angry",1)
-("Wendy",6,"Male","Brown","Angry",1)
-(NULL,8,"Male","Brown","Angry",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr. Chocolate",5,"Male","Grey","",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Rowdy",4,"Femal","Grey","",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Tabitha",8,"Femal","Grey","",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown",9,"Grey","",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Melissa",6,"Femal","Brown","",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Grinch",4,"Femal","Brown","Angry",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Wendy",6,"Male","Brown","Angry",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL,8,"Male","Brown","Angry",0);
